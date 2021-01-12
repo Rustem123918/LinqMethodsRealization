9fb05cb04a04a1551d944120c7f1ddcbc9022c9c
Rustem Saifullin <rustem123918@mail.ru>
2021-01-12 09:27:55 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using MyLinq;
+
+namespace LinqMethodsRealization
+{
+    sealed class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int>(new[] { 1, 2, 5, 3, 2 });
+            var res = list.Take(3);
+            foreach (var e in res)
+                Console.WriteLine(e);
+        }
+    }
+}

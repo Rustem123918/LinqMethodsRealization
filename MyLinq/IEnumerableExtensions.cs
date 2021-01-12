ac85f2a4d8792559db3eb13b2033c509f6572a1a
Rustem Saifullin <rustem123918@mail.ru>
2021-01-12 09:59:21 +0300

    Реализовал методы Where, Select, Take, ToList

---
+++ MyLinq/IEnumerableExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyLinq
+{
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<T> Where<T>(this IEnumerable<T> collection, Predicate<T> predicate)
+        {
+            foreach (var e in collection)
+                if (predicate(e))
+                    yield return e;
+        }
+        public static IEnumerable<Tout> Select<Tin, Tout>(this IEnumerable<Tin> collection, Func<Tin, Tout> selector)
+        {
+            foreach (var e in collection)
+                yield return selector(e);
+        }
+        public static List<T> ToList<T>(this IEnumerable<T> collection)
+        {
+            var result = new List<T>();
+            foreach (var e in collection)
+                result.Add(e);
+            return result;
+        }
+        public static T FirstOrDefault<T>(this IEnumerable<T> collection, Func<T, bool> filter)
+        {
+            foreach (var e in collection)
+                if (filter(e)) return e;
+            return default(T);
+        }
+        public static IEnumerable<T> Take<T>(this IEnumerable<T> collection, int count)
+        {
+            var i = 0;
+            foreach(var e in collection)
+            {
+                if (i >= count)
+                    break;
+                yield return e;
+                i++;
+            }
+        }
+    }
+}
